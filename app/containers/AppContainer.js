1e62a61bd6a06f518737ba894236ba39d50c3e2e
Joshua Lyman <jlyman.work@gmail.com>
2016-03-08 06:15:01 -0600

    Got to a broken state where NavigationCardStack won't render because 'Cannot read property 'width' of undefined
    '

--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -1,25 +1,43 @@
 'use strict'
 
-import React, { Component, View, StyleSheet, PropTypes } from 'react-native'
+import React, { Component, NavigationExperimental, View, StyleSheet, PropTypes } from 'react-native'
+import NavigationRootContainer from 'NavigationRootContainer'
 import { connect } from 'react-redux'
 
 import First from './First'
 import Second from './Second'
 import Third from './Third'
 
+const NavigationCardStack = NavigationExperimental.Card
+
 
 class AppContainer extends Component {
+	componentWillMount() {
+		this._renderNavigation = this._renderNavigation.bind(this)
+		this._renderScene = this._renderScene.bind(this)
+	}
+
 	render() {
-		let screen = this.navRender()
-		
 		return (
-			<View style={styles.outerContainer}>
-				{screen}
-			</View>
+			<NavigationRootContainer 
+				reducer={() => { return { key: 'First' }}}
+				renderNavigation={this._renderNavigation} />
 		)
 	}
 
-	navRender = () => {
+	_renderNavigation(navState, onNavigate) {
+		return (
+			<NavigationCardStack
+				direction="horizontal"
+				navigationState={navState}
+				onNavigate={onNavigate}
+				renderScene={this._renderScene}
+				style={styles.outerContainer} />
+		)
+	}
+
+	_renderScene(props) {
+		console.log('got to renderScene')
 		switch(this.props.currentRoute) {
 		case 'First':
 			return <First />
@@ -28,7 +46,7 @@ class AppContainer extends Component {
 		case 'Third':
 			return <Third />
 		}
-	};
+	}
 }
 
 AppContainer.contextTypes = {
