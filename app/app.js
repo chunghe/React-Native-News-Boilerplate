7e46bfe045be1868e7c7eccc393b47bf1084bf24
chunghe <chunghe@gmail.com>
2016-06-23 14:10:24 +0800

    move redux store logic to `store/configureStore.js`

--- app/app.js
+++ app/app.js
@@ -1,14 +1,10 @@
 import React, {Component} from 'react'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
 
-import reducers from './redux/reducers'
 import AppContainer from './containers/AppContainer'
+import configureStore from './store/configureStore'
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
-const store = createStoreWithMiddleware(reducers)
-console.log('store', store.getState());
+const store = configureStore();
 
 export default class App extends Component {
 	render() {
---
+++ app/store/configureStore.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from '../redux/reducers'
+
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+
+export default function configureStore(initialState = {}) {
+  const store = createStoreWithMiddleware(reducers, initialState);
+  return store;
+}
