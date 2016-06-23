e0a5c5bb39b5b362317c01a35102e77b1f6e74ba
chunghe <chunghe@gmail.com>
2016-06-23 14:42:08 +0800

    debugging with remote-redux-devtools

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,11 +1,25 @@
-import { createStore, applyMiddleware } from 'redux'
+import { Platform } from 'react-native';
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import reducers from '../redux/reducers'
+import devTools from 'remote-redux-devtools';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
 
+let enhancer;
+
+if (__DEV__) {
+  enhancer = compose(
+    devTools({
+      name: Platform.OS,
+      hostname: 'localhost',
+      port: 5678
+    })
+  )
+}
+
 export default function configureStore(initialState = {}) {
-  const store = createStoreWithMiddleware(reducers, initialState);
+  const store = createStoreWithMiddleware(reducers, initialState, enhancer);
 
   if (module.hot) {
     module.hot.accept(() => {
