f3c74742c5302a5e66de40bcdb69041a68c3e2c2
chunghe <chunghe@gmail.com>
2016-07-18 14:14:02 +0800

    upgrade react-native-svg to 3.0.0 to work with RN 0.29

--- android/app/src/main/java/com/navexpredux/MainApplication.java
+++ android/app/src/main/java/com/navexpredux/MainApplication.java
@@ -2,6 +2,7 @@ package com.navexpredux;
 
 import android.app.Application;
 import android.util.Log;
+import com.horcrux.svg.RNSvgPackage;
 
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -23,7 +24,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNSvgPackage()
       );
     }
   };
